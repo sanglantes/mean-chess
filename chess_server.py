43920e437ac0e75aae0fbb8860ee83834a96f281
sanglantes <101125878+sanglantes@users.noreply.github.com>
2023-09-22 04:10:12 +0200

    Update chess_server.py

--- chess_server.py
+++ chess_server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
+import json
 import chess, chess.engine
 
 app = Flask(__name__)
@@ -12,22 +13,36 @@ def algebraic_to_fen(algebraic_notation):
 
     return board.fen()
 
-def get_best_move(fen, depth):
+def get_best_move(fen, depth, discrete):
     board = chess.Board(fen)
-    engine = chess.engine.SimpleEngine.popen_uci("PATH_TO_STOCKFISH")
-    result = engine.play(board, chess.engine.Limit(time=depth))
-    engine.quit()
+    engine = chess.engine.SimpleEngine.popen_uci("stockfish/stockfish-windows-x86-64-avx2.exe")
 
-    return result.move
+    if not discrete:
+        result = engine.play(board, chess.engine.Limit(time=depth))
+        engine.quit()
+
+        return result.move
+
+    elif discrete:
+        analyse = engine.analyse(board, chess.engine.Limit(time=depth), multipv=2)
+        moves = [result["pv"][0].uci() for result in analyse]
+
+        return moves
 
 @app.route('/api', methods=['GET'])
 def api():
-	algebraic_notation = request.args.get("algebra")
-	depth = request.args.get("depth")
+    algebraic_notation = request.args.get("algebra")
+    depth = request.args.get("depth")
+    discrete = int(request.args.get("discrete"))
 
-	best_move = str(get_best_move(algebraic_to_fen(algebraic_notation), float(depth)))
-	best_move = best_move[:2] + ' ' + best_move[2:]
-	return best_move
+    best_move = get_best_move(algebraic_to_fen(algebraic_notation), float(depth), discrete)
+    if not discrete:
+        best_move = str(best_move)
+        best_move = best_move[:2] + ' ' + best_move[2:]
+        return best_move
+
+    elif discrete:
+        return best_move
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
